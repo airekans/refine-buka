8fdebf54c72954e2ca1dcdfa848662156263002e
Dingyuan <abcdoyle888@gmail.com>
2014-06-13 23:30:08 +0800

    Localize.

--- buka.py
+++ buka.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Python 3.x
 
-__version__ = '1.6'
+__version__ = '1.7'
 
 '''
 布卡漫画转换工具
@@ -17,23 +17,22 @@ import shutil
 import time
 import json
 import struct
-import gettext
 from subprocess import Popen
+helpm = '''提取布卡漫画下载的漫画文件
 
-helpm = '''Extract mangas downloaded by Buka.
+用法: python3 %s 输入文件(夹) [输出文件夹]
 
-usage: python3 %s input [output]
+必需参数:
+ 输入文件(夹)  .buka文件或包含下载文件的文件夹
+             通常位于 (安卓SD卡) /sdcard/ibuka/down
 
-positional argument:
- input       the .buka file or a directory contains downloaded files.
-             usually it's in (Android) /sdcard/ibuka/down
-
-optional argument:
- output      the target directory.(= the current dir.)
+可选参数:
+ 输出文件夹    指定输出文件夹
+             默认为原目录下 output 文件夹
 ''' % sys.argv[0]
 
 if sys.version_info[0] < 3:
-	print('Python 3 required.')
+	print('要求 Python 3.')
 	if os.name == 'nt':
 		time.sleep(3)
 	sys.exit()
@@ -64,16 +63,23 @@ if os.name == 'nt':
 else:
 	dwebp = os.path.join(programdir, 'dwebp')
 
-print("Checking environment...")
+print("检查环境...")
 try:
 	with open(os.devnull, 'w') as nul:
 		p = Popen(dwebp, stdout=nul, stderr=nul).wait()
 	supportwebp = True
 except Exception as ex:
-	print(
-            "dwebp is not available, so only normal image files are supported.\n" + repr(ex))
-	supportwebp = False
-
+	if os.name == 'posix':
+		try:
+			with open(os.devnull, 'w') as nul:
+				p = Popen('dwebp', stdout=nul, stderr=nul).wait()
+			supportwebp = True
+		except Exception as ex:
+			print(_("dwebp 不可用，仅支持普通文件格式。\n") + repr(ex))
+			supportwebp = False
+	else:
+		print(_("dwebp 不可用，仅支持普通文件格式。\n") + repr(ex))
+		supportwebp = False
 
 def copytree(src, dst, symlinks=False, ignore=None):
 	if not os.path.exists(dst):
@@ -91,13 +97,11 @@ def copytree(src, dst, symlinks=False, ignore=None):
 			elif not os.path.isfile(d) or os.stat(src).st_mtime - os.stat(dst).st_mtime > 1:
 				shutil.copy2(s, d)
 
-
 def build_dict(seq, key):
 	rd = {}
 	for d in seq:
 		rd[d[key]] = d
 	return rd
-
 
 def renamef(chap, cid):
 	if chap.get(cid):
@@ -115,10 +119,9 @@ def renamef(chap, cid):
 	else:
 		return cid
 
-
 def extractbuka(bkname, target):
 	if not os.path.isfile(bkname):
-		print('No such file: ' + bkname)
+		print('没有此文件: ' + bkname)
 		return ''
 	if not os.path.exists(target):
 		os.mkdir(target)
@@ -126,6 +129,7 @@ def extractbuka(bkname, target):
 	comicname = ''
 	with open(bkname, 'rb') as f:
 		buff = f.read(16384)
+		chapid = struct.unpack('<I', buff[16:20])[0]
 		pos = buff.find(b'\x00', 20)
 		comicname = buff[20:pos].decode(errors='ignore')
 		pos += 1
@@ -147,48 +151,46 @@ def extractbuka(bkname, target):
 if os.path.isdir(target):
 	if os.path.splitext(fn_buka)[1] == ".buka":
 		if not os.path.isfile(fn_buka):
-			print('No such file: ' + fn_buka)
+			print('没有此文件: ' + fn_buka)
 			if not os.listdir(target):
 				os.rmdir(target)
 			if os.name == 'nt':
 				time.sleep(3)
 			sys.exit()
-		print(('Extracting %s' % fn_buka))
+		print('正在提取 ' + fn_buka)
 		extractbuka(fn_buka, target)
 		if os.path.isfile(os.path.join(target, "chaporder.dat")):
 			dat = json.loads(open(os.path.join(target, "chaporder.dat"), 'r').read())
 			os.remove(os.path.join(target, "chaporder.dat"))
 			chap = build_dict(dat['links'], 'cid')
-			newtarget = os.path.join(os.path.dirname(target), dat[
-			                         'name'] + '-' + renamef(chap, os.path.basename(os.path.splitext(fn_buka)[0])))
+			newtarget = os.path.join(os.path.dirname(target), dat['name'] + '-' + renamef(chap, os.path.basename(os.path.splitext(fn_buka)[0])))
 			shutil.move(target, newtarget)
 			target = newtarget
 	elif os.path.isdir(fn_buka):
-		print('Copying...')
+		print('正在复制...')
 		copytree(fn_buka, target)
 	else:
-		print("Input must be a .buka file or a folder.")
+		print("输入必须为 .buka 文件或一个文件夹。")
 	allfile = []
 	dwebps = []
 	for root, subFolders, files in os.walk(target):
 		for name in files:
 			fpath = os.path.join(root, name)
 			if os.path.splitext(fpath)[1] == ".buka":
-				print(('Extracting %s' % fpath))
+				print('正在提取 ' + fpath)
 				extractbuka(fpath, os.path.splitext(fpath)[0])
 				chaporder = os.path.join(os.path.splitext(fpath)[0], "chaporder.dat")
 				if os.path.isfile(chaporder):
 					dat = json.loads(open(chaporder, 'r').read())
 					os.remove(chaporder)
 					chap = build_dict(dat['links'], 'cid')
-					shutil.move(os.path.splitext(fpath)[0], os.path.join(os.path.dirname(
-                                            fpath), renamef(chap, os.path.basename(os.path.splitext(fpath)[0]))))
+					shutil.move(os.path.splitext(fpath)[0], os.path.join(os.path.dirname(fpath), renamef(chap, os.path.basename(os.path.splitext(fpath)[0]))))
 				os.remove(fpath)
 	for root, subFolders, files in os.walk(target):
 		for name in files:
 			allfile.append(os.path.join(root, name))
 	for fpath in allfile:
-		print(('Extracting %s' % fpath))
+		print('正在提取 ' + fpath)
 		if os.path.splitext(fpath)[1] in (".view", ".bup"):
 			if os.path.splitext(fpath)[1] == ".view":
 				bupname = os.path.splitext(fpath)[0]
@@ -201,8 +203,7 @@ if os.path.isdir(target):
 						bup.read(64)  # and eat it
 						shutil.copyfileobj(bup, webp)
 					os.remove(fpath)
-					p = Popen([dwebp, basepath + ".webp", "-o", os.path.splitext(basepath)
-                                            [0] + ".png"], cwd=os.getcwd())  # .wait()  faster
+					p = Popen([dwebp, basepath + ".webp", "-o", os.path.splitext(basepath)[0] + ".png"], cwd=os.getcwd())  # .wait()  faster
 					time.sleep(0.2)  # prevent creating too many dwebp's
 					if not p.poll():
 						dwebps.append(p)
@@ -212,11 +213,11 @@ if os.path.isdir(target):
 				shutil.move(fpath, bupname)
 		# else:	pass
 	if dwebps:
-		print("Waiting for all dwebp's...")
+		print("等待所有 dwebp 转换进程...")
 		for p in dwebps:
 			p.wait()
-	print("Convertion done.")
-	print("Renaming...")
+	print("完成转换。")
+	print("正在重命名...")
 	alldir = []
 	for root, subFolders, files in os.walk(target):
 		for name in files:
@@ -233,11 +234,10 @@ if os.path.isdir(target):
 			chap = build_dict(dat['links'], 'cid')
 			for item in os.listdir(fpath):
 				if os.path.isdir(os.path.join(fpath, item)):
-					shutil.move(
-                                            os.path.join(fpath, item), os.path.join(fpath, renamef(chap, item)))
+					shutil.move(os.path.join(fpath, item), os.path.join(fpath, renamef(chap, item)))
 			shutil.move(fpath, os.path.join(dirname[0], dat['name']))
 	if not supportwebp:
-		print('WARNING: .bup files are not extracted.')
-	print('Done.')
+		print('警告: .bup 格式文件无法提取。')
+	print('完成。')
 else:
-	print("Output put must be a folder.")
+	print("错误: 输出文件夹路径为一个文件。")
