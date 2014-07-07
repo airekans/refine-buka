770fc5916610798d58653b905f2f5190e83cd402
Dingyuan Wang <abcdoyle888@gmail.com>
2014-07-07 12:09:57 +0800

    don't include dwebp

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building...
-C:\Python33\Scripts\cxfreeze --target-dir=dist -OO --zip-include=dwebp.exe --icon=buka.ico buka.py
+C:\Python33\Scripts\cxfreeze --target-dir=dist -OO --icon=buka.ico buka.py
 echo.
 echo Done.
 pause
