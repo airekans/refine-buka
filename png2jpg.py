b0dd3a36e00e2f1b3a4a0331c9df5e94f879300f
Dingyuan Wang <abcdoyle888@gmail.com>
2014-07-15 14:59:04 +0800

    improve jpg quality

--- png2jpg.py
+++ png2jpg.py
@@ -23,7 +23,7 @@ def main():
 			if os.path.splitext(name)[1] == '.png':
 				print(os.path.join(root, name))
 				im = Image.open(os.path.join(root, name))
-				im.save(os.path.splitext(os.path.join(root, name))[0] + '.jpg', quality=90)
+				im.save(os.path.splitext(os.path.join(root, name))[0] + '.jpg', quality=95)
 				im.close()
 				del im
 				tryremove(os.path.join(root, name))
