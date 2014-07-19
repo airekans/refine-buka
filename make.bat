b405ba68f4361bff686d5d5c4909b40e80b4987e
Dingyuan Wang <abcdoyle888@gmail.com>
2014-07-19 15:02:53 +0800

    update the Windows build  file.

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Building...
-start C:\Python33\Scripts\cxfreeze --target-dir=dist -OO png2jpg.py
-C:\Python33\Scripts\cxfreeze --target-dir=dist -OO --icon=buka.ico buka.py
+C:\Python33\python.exe C:\Python33\Scripts\cxfreeze --target-dir=dist -OO --exclude-modules=PIL,PIL.Image,Image --icon=buka.ico buka.py
+C:\Python33\python.exe C:\Python33\Scripts\cxfreeze --target-dir=dist -OO png2jpg.py
 echo.
 echo Done.
 pause
