e204992f063b95d62b3e4a17753815b2106a9fc0
Dingyuan Wang <abcdoyle888@gmail.com>
2015-02-03 13:04:21 +0800

    experimental downloader

--- buka.py
+++ buka.py
@@ -32,6 +32,7 @@ import time
 import json
 import struct
 import sqlite3
+import urllib.request, urllib.parse
 import logging, logging.config
 import traceback
 import threadpool
@@ -1084,6 +1085,53 @@ class DwebpSingleThreadPILMan:
 				return False
 			else:
 				raise ex
+
+def downloader(comicid, chapid, path='.'):
+	"""
+	Experimental Buka doownloader.
+	Supports buka file only.
+
+	Current findings are listed as follow:
+
+	There is a JSON (RPC-like) interface at
+	http://cs.bukamanhua.com:8000/request.php?t=%d
+	where we can POST a JSON function.
+	So far we have found these:
+	('func_getsimpleinfo', 'func_getdetail', 'func_getdownurl3', 'func_getglobalvar', 'func_recomhome3')
+	The response is a JSON object with 'ret' value guaranteed. 0 for success.
+
+	To download corresponding index2.dat, GET
+	'http://index.bukamanhua.com:8000/req3.php?mid=%s&cid=%s&c=f76b8c7a03490e06bb3389544bc527a5&s=ad&v=5&t=-1&restype=2&cv=17301551' % (comicid, chapid)
+
+	In index2.dat there is a gzipped (b'\x1f\x8b') JSON object, like this:
+	{"resbk":"http:\\/\\/c-pic3.weikan.cn\\/pich","resbklist":["http:\\/\\/c-r2.sosobook.cn\\/pich","http:\\/\\/c-pic3.weikan.cn\\/pich"],"idxver":"137960966","restype":2}
+	"""
+	if os.path.isdir(path):
+		path = os.path.join(path, '%s.buka' % chapid)
+	postdata = ('i=%s&z=0&p=android&v=9&c=91643f635a86aad35b9f942db576f233' % urllib.parse.quote(urllib.parse.quote(json.dumps({"f":"func_getdownurl3","ver":3,"mid":comicid,"cid":chapid,"restype":2})))).encode('utf-8')
+	req = urllib.request.Request("http://cs.bukamanhua.com:8000/request.php?t=%d" % time.time(), data=postdata, headers={"Content-Type": "application/x-www-form-urlencoded"})
+	downlist = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
+	if downlist['ret']:
+		return False
+	downlist = downlist["down"]
+	defaultheader = {
+		'Accept-Encoding': 'gzip,deflate',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.12 (KHTML, like Gecko) Maxthon/3.3.4.4000 Chrome/18.0.966.0 Safari/535.12',
+		'Connection': 'Keep-Alive',
+	}
+	for obj in downlist:
+		print(downlist)
+		if int(obj["urltype"]) == 1:
+			# It seems that we need to parse index2.dat to get download url.
+			# Download index2.dat from this url:
+			raise NotImplementedError('directory type with url: ' + obj["url"])
+		elif int(obj["urltype"]) == 2:
+			url = obj["url"]
+			defaultheader['refer'] = url
+			defaultheader['Host'] = urllib.parse.urlsplit(url)[1]
+			req = urllib.request.Request(url, headers=defaultheader)
+			shutil.copyfileobj(urllib.request.urlopen(req), open(path, 'wb'))
+	return False
 
 def logexit(err=True, wait=True):
 	logging.shutdown()
